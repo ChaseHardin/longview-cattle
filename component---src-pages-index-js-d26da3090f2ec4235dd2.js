12f93a7e371a5eb33fb3db9f45aea9bd31fbe4a4
Chase Hard <dchasehardin@gmail.com>
2022-04-23 16:26:56 -0500

    Updates

---
+++ component---src-pages-index-js-d26da3090f2ec4235dd2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklongview_cattle=self.webpackChunklongview_cattle||[]).push([[678],{558:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var l=t(294);function u(){return l.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d26da3090f2ec4235dd2.js.map
