d8ec4fa8724a184906491d7fb9bd55867510873f
Chase Hard <dchasehardin@gmail.com>
2022-04-23 23:19:30 -0500

    Make things we offer responsive

--- src/components/things-we-offer-component.js
+++ src/components/things-we-offer-component.js
@@ -1,26 +1,27 @@
 import React from "react"
 import VerifiedIcon from "@mui/icons-material/Verified"
+import { Grid } from "@mui/material"
 
 import "../styles/things-we-offer.css"
 
 export const ThingsWeOffer = () => {
   return (
-    <section>
+    <section className="buffer">
       <h1 className="things-we-offer-header">{"Everything We Offer"}</h1>
-      <div className="things-we-offer-container">
+      <Grid container className="things-we-offer-container">
         <StyledVerification text={"Beef"} />
         <StyledVerification text={"Sires"} />
         <StyledVerification text={"Donors"} />
-      </div>
+      </Grid>
     </section>
   )
 }
 
 const StyledVerification = props => {
   return (
-    <div className="verification-icon">
+    <Grid item xs={12} md={4} className="verification-icon">
       <VerifiedIcon className="verified-icon-style" />
       <div className="verified-icon">{props.text}</div>
-    </div>
+    </Grid>
   )
 }
