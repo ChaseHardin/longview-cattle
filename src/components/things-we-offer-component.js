44127e30763ba9717e57c1b57e065c1d5a9194da
Chase Hard <dchasehardin@gmail.com>
2022-04-23 23:12:23 -0500

    Add header to things we offer

--- src/components/things-we-offer-component.js
+++ src/components/things-we-offer-component.js
@@ -5,19 +5,22 @@ import "../styles/things-we-offer.css"
 
 export const ThingsWeOffer = () => {
   return (
-    <section className="things-we-offer-container">
-      <StyledVerification text={"Beef"} />
-      <StyledVerification text={"Sires"} />
-      <StyledVerification text={"Donors"} />
+    <section>
+      <h1 className="things-we-offer-header">{"Everything We Offer"}</h1>
+      <div className="things-we-offer-container">
+        <StyledVerification text={"Beef"} />
+        <StyledVerification text={"Sires"} />
+        <StyledVerification text={"Donors"} />
+      </div>
     </section>
   )
 }
 
 const StyledVerification = props => {
   return (
-    <div className='verification-icon'>
-        <VerifiedIcon className="verified-icon-style"/>
-        <div className='verified-icon'>{props.text}</div>
+    <div className="verification-icon">
+      <VerifiedIcon className="verified-icon-style" />
+      <div className="verified-icon">{props.text}</div>
     </div>
   )
 }
