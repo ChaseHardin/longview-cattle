2b696b38c27143067ff581b8a91bb4ec1907541e
Chase Hard <dchasehardin@gmail.com>
2022-04-26 20:58:24 -0500

    styled form

--- src/components/contact-component.js
+++ src/components/contact-component.js
@@ -1,67 +1,86 @@
 import React from "react"
-import { Grid } from "@mui/material"
-import LocationOnIcon from "@mui/icons-material/LocationOn"
-import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone"
+import { Button, Grid } from "@mui/material"
 import EmailIcon from "@mui/icons-material/Email"
+import { TextField } from "@mui/material"
+import { AccountCircle } from "@mui/icons-material"
+import { Box } from "@mui/material"
 
 import "../styles/contact.css"
 
-export const Contact = () => {
-  const handleSubmit = event => {
-    event.preventDefault()
-  }
+// const handleSubmit = event => {
+//   event.preventDefault()
+// }
 
+export const Contact = () => {
   return (
-    <form name="Contact Form" method="POST" data-netlify="true" style={{marginTop: '6rem'}}>
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
-    </form>
+    <Grid container display={"flex"}>
+      <Grid xs={12} sm={4}>
+        <div className="contact-container">
+          <div className="text">
+            <div>Don Hardin</div>
+            <div>515-313-8982</div>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={12} sm={8}>
+        <form
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          style={{ marginTop: "6rem" }}
+        >
+          <input type="hidden" name="form-name" value="Contact Form" />
+
+          <Box
+            sx={{ display: "flex", alignItems: "flex-end", padding: "1rem" }}
+          >
+            <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+            <TextField
+              fullWidth
+              id="input-with-sx"
+              label="Full name"
+              variant="standard"
+            />
+          </Box>
+
+          <Box
+            sx={{ display: "flex", alignItems: "flex-end", padding: "1rem" }}
+          >
+            <EmailIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+            <TextField
+              required
+              fullWidth
+              id="input-with-sx"
+              label="Email"
+              variant="standard"
+              color="primary"
+            />
+          </Box>
+
+          <Box
+            sx={{ display: "flex", alignItems: "flex-end", padding: "1rem" }}
+          >
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              label="Message"
+              multiline
+              rows={12}
+            />
+          </Box>
+          <Box
+            sx={{ display: "flex", alignItems: "flex-end", padding: "1rem" }}
+          >
+            <Button
+              type={"submit"}
+              size={"large"}
+              variant={"contained"}
+            >
+              Send Email
+            </Button>
+          </Box>
+        </form>
+      </Grid>
+    </Grid>
   )
-  // return (
-  //   <Grid container marginTop={"2rem"} display={"flex"}>
-  //     <Grid xs={12} sm={4}>
-  //       <div className="contact-container">
-  //         <div className="icon-container">
-  //           <LocationOnIcon className="lg-icon" />
-  //           <div className="lg-font">Address</div>
-  //         </div>
-  //         <div className="text">
-  //           <div>3220 Wyoming Street</div>
-  //           <div>New Virginia, IA 50210</div>
-  //         </div>
-  //       </div>
-  //     </Grid>
-  //     <Grid xs={12} sm={4}>
-  //       <div className="contact-container">
-  //         <div className="icon-container">
-  //           <PhoneIphoneIcon className="lg-icon" />
-  //           <div className="lg-font">Phone</div>
-  //         </div>
-  //         <div className="text">
-  //           <div>Don Hardin</div>
-  //           <div>515-313-8982</div>
-  //         </div>
-  //       </div>
-  //     </Grid>
-  //     <Grid xs={12} sm={4}>
-  //       <div className="contact-container">
-  //         <div className="icon-container">
-  //           <EmailIcon className="lg-icon" />
-  //           <div className="lg-font">Email</div>
-  //         </div>
-  //         <div className="text">
-  //           <div>donhardin@gmail.com</div>
-  //         </div>
-  //       </div>
-  //     </Grid>
-  //   </Grid>
-  // )
 }
